50ff4ce9594f807081c5034c29024e4c25d906ef
heather-han <heather.h.1122@gmail.com>
2017-04-27 02:31:20 -0400

    Added bash script for running the project. Added plot method

--- svm.py
+++ svm.py
@@ -8,82 +8,81 @@
 import sys
 import numpy as np
 from sklearn import svm
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import cross_val_predict
+import matplotlib.pyplot as plt
 
 
 def main():
 	''' Read data file '''
-	data = np.genfromtxt(sys.argv[1],delimiter=' ')
-	metastasis = np.genfromtxt(sys.argv[2],delimiter=' ')
-
-	print("Distinct Partition")
-	train_x, test_x, train_y, test_y = distinctPartion(data, metastasis)
-	SVM(train_x, test_x, train_y, test_y)
-
-	print("\nConventional Partition")
-	train_x, test_x, train_y, test_y = conventionalPartition(data, metastasis)
-	SVM(train_x, test_x, train_y, test_y)
-
-
-''' Partitioning method 1 '''
-def distinctPartion(X,Y):
-	# Calculate the number of bone metastasis samples
-	num_true = int(np.sum(Y))
-	num_genes = X.shape[0]
-	met_true = np.zeros((num_genes,num_true))
-	met_false = np.zeros((num_genes,len(Y)-num_true))
-
-	true_counter = 0
-	false_counter = 0
-
-	for i in range(0,len(Y)):
-		if Y[i] == 1:
-			met_true[:,true_counter] = X[:,i]
-			true_counter += 1
-		else:
-			met_false[:,false_counter] = X[:,i]
-			false_counter += 1
+	distinct_train_x = np.genfromtxt(sys.argv[1],delimiter=' ')
+	distinct_test_x = np.genfromtxt(sys.argv[2],delimiter=' ')
+	distinct_train_y = np.genfromtxt(sys.argv[3],delimiter=' ')
+	distinct_test_y = np.genfromtxt(sys.argv[4],delimiter=' ')
+	conv_train_x = np.genfromtxt(sys.argv[5],delimiter=' ')
+	conv_test_x = np.genfromtxt(sys.argv[6],delimiter=' ')
+	conv_train_y = np.genfromtxt(sys.argv[7],delimiter=' ')
+	conv_test_y = np.genfromtxt(sys.argv[8],delimiter=' ')
 	
-	#partion met_true and met_false by 70%
-	split_true = int(met_true.shape[1]*0.7) #69*0.7=48
-	split_false = int(met_false.shape[1]*0.7) #217*0.7 = 151
-	train_size = split_false+split_true
-	#for test file
-	split_true_test = int(met_true.shape[1] - split_true) 
-	split_false_test = int(met_false.shape[1] - split_false)
+	results = {}
 
-	train_x = np.zeros((train_size, num_genes))
-	test_x = np.zeros((X.shape[1] - train_size, num_genes))
+	print "Distinct Partition without Feature Selection:"
+	result = SVM(distinct_train_x, distinct_test_x, distinct_train_y, distinct_test_y)
+	results["distinct"] = result
 
-	met_true_trans = met_true.T 
-	met_false_trans = met_false.T
+	print "\nConventional Partition without Feature Selection:"
+	result = SVM(conv_train_x, conv_test_x, conv_train_y, conv_test_y)
+	results["conventional"] = result
 
-	train_x[:split_true, :] = met_true_trans[:split_true, :]
-	train_x[split_true:, :] = met_false_trans[:split_false, :]
-	
-	test_x[:split_true_test, :] = met_true_trans[split_true:, :]
-	test_x[split_true_test:, :] = met_false_trans[split_false:, :]
+	''' Feature Selection '''
+	print "\nDistinct Partition with Feature Selection:"
+	distinct_train_new, distinct_test_new = selectFeatures(distinct_train_x,distinct_test_x,distinct_train_y,distinct_test_y)
+	result = SVM(distinct_train_new, distinct_test_new, distinct_train_y, distinct_test_y)
+	results["distinctF"] = result
 
-	train_y = [1]*split_true + [0]*split_false
-	test_y = [1]*split_true_test + [0]*split_false_test
+	print "\nConventional Partition with Feature Selection:"
+	conv_train_new, conv_test_new = selectFeatures(conv_train_x,conv_test_x,conv_train_y,conv_test_y)
+	result = SVM(conv_train_new, conv_test_new, conv_train_y, conv_test_y)
+	results["conventionalF"] = result
 
 	
-	return train_x, test_x, train_y, test_y
+	for i in results:
+		count = 1
+		for j in results[i]:
+			if i == 'distinct':
+				color = 'b'
+				label = "Distinct"
+			if i == 'conventional':
+				color = 'y'
+				label = "Conventional"
+			if i == 'distinctF':
+				color = 'r'
+				label = "Feature Select -- Distinct"
+			if i == 'conventionalF':
+				color = 'purple'
+				label = "Feature Select -- Conventional"
+			plt.scatter(count, j, c = color, label=label)
+			count += 1
+	# colors = ['navy', 'darkorange', 'red', 'yellow']
+	# labels = ["Distinct","Conventional","Feature Select -- Distinct","Feature Select -- Conventional"]
+
+	# for result in results:
+	# 	for i, color, labels in zip([1,2,3,4,5], colors, labels):
+	# 		plt.scatter(i,result[disease == i, 1],color=color,label=labels)
+
+	plt.legend(loc='best', scatterpoints=1)
+	plt.show()
 
 
-''' Partitioning method 2 '''
-def conventionalPartition(X,Y):
-	#random partition by 70:30
-	(samples, genes) = X.T.shape
-	train_samples = int(samples*0.7)
-	train_x = X.T[:train_samples, :]
-	train_y = Y[:train_samples]
-
-	test_x = X.T[train_samples:, :]
-	test_y = Y[train_samples:]
-
-	return train_x, test_x, train_y, test_y
+def selectFeatures(train_x, test_x, train_y, test_y):
+	clf = ExtraTreesClassifier()
+	clf = clf.fit(train_x, train_y)
+	model = SelectFromModel(clf, prefit = True)
+	train_new = model.transform(train_x)
+	test_new = model.transform(test_x)
+	return train_new, test_new
 
 
 def SVM(train_x, test_x, train_y, test_y):
@@ -92,31 +91,31 @@ def SVM(train_x, test_x, train_y, test_y):
 	lin_svm = svm.SVC(kernel='linear').fit(train_x, train_y)
 	lin_accuracy = lin_svm.score(train_x, train_y)
 	lin_accuracy_test = lin_svm.score(test_x, test_y)
-	lin_cv = cross_val_score(lin_svm, train_x, train_y, cv=5).mean()
+	lin_cv = cross_val_score(lin_svm, train_x, train_y, cv=10).mean()
 	
 	# Gaussian (RBF) kernel SVM
 	gau_svm = svm.SVC(kernel='rbf').fit(train_x, train_y)
 	gau_accuracy = gau_svm.score(train_x, train_y)
 	gau_accuracy_test = gau_svm.score(test_x, test_y)
-	gau_cv = cross_val_score(gau_svm, train_x, train_y, cv=5).mean()
+	gau_cv = cross_val_score(gau_svm, train_x, train_y, cv=10).mean()
 
 	# Polynomial kernel SVM with d = 3
 	pol3_svm = svm.SVC(kernel='poly',degree=3).fit(train_x, train_y)
 	pol3_accuracy = pol3_svm.score(train_x, train_y)
 	pol3_accuracy_test = pol3_svm.score(test_x, test_y)
-	pol3_cv = cross_val_score(pol3_svm, train_x, train_y, cv=5).mean()
+	pol3_cv = cross_val_score(pol3_svm, train_x, train_y, cv=10).mean()
 
 	# Polynomial kernel SVM with d = 4
 	pol4_svm = svm.SVC(kernel='poly',degree=4).fit(train_x, train_y)
 	pol4_accuracy = pol4_svm.score(train_x, train_y)
 	pol4_accuracy_test = pol4_svm.score(test_x, test_y)
-	pol4_cv = cross_val_score(pol4_svm, train_x, train_y, cv=5).mean()
+	pol4_cv = cross_val_score(pol4_svm, train_x, train_y, cv=10).mean()
 
 	# Polynomial kernel SVM with d = 6
 	pol6_svm = svm.SVC(kernel='poly',degree=6).fit(train_x, train_y)
 	pol6_accuracy = pol6_svm.score(train_x, train_y)
 	pol6_accuracy_test = pol6_svm.score(test_x, test_y)
-	pol6_cv = cross_val_score(pol6_svm, train_x, train_y, cv=5).mean()
+	pol6_cv = cross_val_score(pol6_svm, train_x, train_y, cv=10).mean()
 
 	''' Accuracy without cross validation '''
 	print "Accuracy without Cross Validation: "
@@ -130,11 +129,12 @@ def SVM(train_x, test_x, train_y, test_y):
 	print "Polynomial accuracy with d=3,4,6: ", pol3_accuracy_test,pol4_accuracy_test,pol6_accuracy_test
 	
 	''' Accuracy with 5-fold cross validation '''
-	print "\nAccuracy with 5-fold Cross Validation: "
+	print "\nAccuracy with 10-fold Cross Validation: "
 	print "Linear kernel accuracy: ", lin_cv
 	print "Gaussian kernel accuracy: ", gau_cv
 	print "Polynomial accuracy with d=3,4,6: ", pol3_cv, pol4_cv, pol6_cv
 
+	return lin_cv, gau_cv, pol3_cv, pol4_cv, pol6_cv
 
 
 if __name__ == '__main__':
