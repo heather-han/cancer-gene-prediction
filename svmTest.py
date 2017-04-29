9e2d210717bec9fd9f2930cceb8e6ad98e7689ed
Katherine Wu <jwu86@jhu.edu>
2017-04-29 18:19:02 -0400

    svmTrain and svmTest works now

---
+++ svmTest.py
@@ -0,0 +1,138 @@
+# Heather Han (hhan16)
+# Jiayao Wu (jwu86)
+# 29 April 2017
+
+# python version: 2.7
+
+import sys
+import numpy as np
+from sklearn import svm
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+import pickle
+
+
+def main():
+	''' Read data file '''
+	distinct_train_x = np.genfromtxt(sys.argv[1],delimiter=' ')
+	distinct_test_x = np.genfromtxt(sys.argv[2],delimiter=' ')
+	distinct_train_y = np.genfromtxt(sys.argv[3],delimiter=' ')
+	distinct_test_y = np.genfromtxt(sys.argv[4],delimiter=' ')
+	conv_train_x = np.genfromtxt(sys.argv[5],delimiter=' ')
+	conv_test_x = np.genfromtxt(sys.argv[6],delimiter=' ')
+	conv_train_y = np.genfromtxt(sys.argv[7],delimiter=' ')
+	conv_test_y = np.genfromtxt(sys.argv[8],delimiter=' ')
+	resultsFolder = sys.argv[9]
+	results = {}
+
+	print "********************* WITHOUT FEATURE SELECTION **********************"
+	print "\n********** Distinct Partition without Feature Selection: **********"
+	result = SVM(distinct_train_x, distinct_test_x, distinct_train_y, distinct_test_y, resultsFolder+'/distinct/')
+	results["distinct"] = result
+
+	print "\n********** Conventional Partition without Feature Selection: **********"
+	result = SVM(conv_train_x, conv_test_x, conv_train_y, conv_test_y, resultsFolder+'/conventional/')
+	results["conventional"] = result
+
+	''' Feature Selection '''
+	print "\n********************* WITH FEATURE SELECTION **********************"
+	print "\n*********** Distinct Partition with Feature Selection: **********"
+	distinct_train_new = np.genfromtxt(sys.argv[10],delimiter=' ')
+	distinct_test_new = np.genfromtxt(sys.argv[11],delimiter=' ')
+	result = SVM(distinct_train_new, distinct_test_new, distinct_train_y, distinct_test_y, resultsFolder+'/feature_distinct/')
+	results["distinctF"] = result
+
+	print "\n********** Conventional Partition with Feature Selection: ***********"
+	conv_train_new = np.genfromtxt(sys.argv[12],delimiter=' ')
+	conv_test_new = np.genfromtxt(sys.argv[13],delimiter=' ')
+
+	result = SVM(conv_train_new, conv_test_new, conv_train_y, conv_test_y, resultsFolder+'/feature_conventional/')
+	results["conventionalF"] = result
+
+
+	for i in results:
+		count = 1
+		for j in results[i]:
+			if i == 'distinct':
+				plt.scatter(count, j, c = 'g', marker='*', alpha=0.7, s=60, label='Distinct')
+			if i == 'conventional':
+				plt.scatter(count, j, c = 'navy', marker='^', alpha=0.7, s=60, label='Conventional')
+			if i == 'distinctF':
+				plt.scatter(count, j, c = 'r', marker='p', alpha=0.7, s=60, label="Feature Select -- Distinct")
+			if i == 'conventionalF':
+				plt.scatter(count, j, c = 'purple', marker='o', alpha=0.7, s=60, label="Feature Select -- Conventional")
+			count += 1
+
+	handles, labels = plt.gca().get_legend_handles_labels()
+	by_label = OrderedDict(zip(labels, handles))
+	plt.legend(by_label.values(), by_label.keys())
+
+	xticks = ['linear','gaussian','pol3','pol4','pol6']
+	plt.xticks([1,2,3,4,5],xticks)
+	plt.title("SVM Classification Accuracy")
+	plt.ylabel("Accuracy")
+	plt.xlabel("Method")
+	plt.show()
+
+
+#def dimensionReduction(train_x, test_x):
+	
+
+def SVM(train_x, test_x, train_y, test_y, resultsFolder):
+	''' Generate a SVM for each model '''
+	# linear kernel SVM
+	lin_svm = pickle.load(open(resultsFolder+'linear.txt', 'rb'))
+	lin_accuracy = lin_svm.score(train_x, train_y)
+	lin_accuracy_test = lin_svm.score(test_x, test_y)
+	lin_cv = cross_val_score(lin_svm, train_x, train_y, cv=10).mean()
+	
+	# Gaussian (RBF) kernel SVM
+	gau_svm = pickle.load(open(resultsFolder+'gaussian.txt', 'rb'))
+	gau_accuracy = gau_svm.score(train_x, train_y)
+	gau_accuracy_test = gau_svm.score(test_x, test_y)
+	gau_cv = cross_val_score(gau_svm, train_x, train_y, cv=10).mean()
+
+	# Polynomial kernel SVM with d = 3
+	pol3_svm = pickle.load(open(resultsFolder+'pol3.txt', 'rb'))
+	pol3_accuracy = pol3_svm.score(train_x, train_y)
+	pol3_accuracy_test = pol3_svm.score(test_x, test_y)
+	pol3_cv = cross_val_score(pol3_svm, train_x, train_y, cv=10).mean()
+
+	# Polynomial kernel SVM with d = 4
+	pol4_svm = pickle.load(open(resultsFolder+'pol4.txt', 'rb'))
+	pol4_accuracy = pol4_svm.score(train_x, train_y)
+	pol4_accuracy_test = pol4_svm.score(test_x, test_y)
+	pol4_cv = cross_val_score(pol4_svm, train_x, train_y, cv=10).mean()
+
+	# Polynomial kernel SVM with d = 6
+	pol6_svm = pickle.load(open(resultsFolder+'pol6.txt', 'rb'))
+	pol6_accuracy = pol6_svm.score(train_x, train_y)
+	pol6_accuracy_test = pol6_svm.score(test_x, test_y)
+	pol6_cv = cross_val_score(pol6_svm, train_x, train_y, cv=10).mean()
+
+	''' Accuracy without cross validation '''
+	print "Accuracy without Cross Validation: "
+	print "Linear kernel accuracy: ", lin_accuracy
+	print "Gaussian kernel accuracy: ", gau_accuracy
+	print "Polynomial accuracy with d=3,4,6: ", pol3_accuracy,pol4_accuracy,pol6_accuracy
+
+	print "\nAccuracy without Cross Validation for testing files: "
+	print "Linear kernel accuracy: ", lin_accuracy_test
+	print "Gaussian kernel accuracy: ", gau_accuracy_test
+	print "Polynomial accuracy with d=3,4,6: ", pol3_accuracy_test,pol4_accuracy_test,pol6_accuracy_test
+	
+	''' Accuracy with 5-fold cross validation '''
+	print "\nAccuracy with 10-fold Cross Validation: "
+	print "Linear kernel accuracy: ", lin_cv
+	print "Gaussian kernel accuracy: ", gau_cv
+	print "Polynomial accuracy with d=3,4,6: ", pol3_cv, pol4_cv, pol6_cv
+
+	return lin_cv, gau_cv, pol3_cv, pol4_cv, pol6_cv
+
+
+if __name__ == '__main__':
+	main()
---
+++ svmTrain.py
@@ -0,0 +1,83 @@
+# Heather Han (hhan16)
+# Jiayao Wu (jwu86)
+# 29 April 2017
+
+# python version: 2.7
+
+import sys
+import numpy as np
+from sklearn import svm
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_predict
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+import pickle
+#path saved: results/params/distinct... /conventional... etc.
+
+def main():
+	''' Read data file '''
+	distinct_train_x = np.genfromtxt(sys.argv[1],delimiter=' ')
+	distinct_test_x = np.genfromtxt(sys.argv[2],delimiter=' ')
+	distinct_train_y = np.genfromtxt(sys.argv[3],delimiter=' ')
+	distinct_test_y = np.genfromtxt(sys.argv[4],delimiter=' ')
+	conv_train_x = np.genfromtxt(sys.argv[5],delimiter=' ')
+	conv_test_x = np.genfromtxt(sys.argv[6],delimiter=' ')
+	conv_train_y = np.genfromtxt(sys.argv[7],delimiter=' ')
+	conv_test_y = np.genfromtxt(sys.argv[8],delimiter=' ')
+	results = sys.argv[9]
+
+	SVM(distinct_train_x, distinct_train_y, "distinct", results)
+
+	SVM(conv_train_x, conv_train_y, "conventional",results)
+
+	''' Feature Selection '''
+	distinct_train_new = selectFeatures(distinct_train_x,distinct_test_x,distinct_train_y,distinct_test_y, "feature_distinct",results)
+	SVM(distinct_train_new, distinct_train_y, "feature_distinct",results)
+
+	conv_train_new = selectFeatures(conv_train_x,conv_test_x,conv_train_y,conv_test_y, "feature_conventional", results)
+	SVM(conv_train_new, conv_train_y, "feature_conventional",results)
+
+
+
+def selectFeatures(train_x, test_x, train_y, test_y, name, results):
+	clf = ExtraTreesClassifier()
+	clf = clf.fit(train_x, train_y)
+	model = SelectFromModel(clf, prefit = True)
+	train_new = model.transform(train_x)
+	test_new = model.transform(test_x)
+	np.savetxt(results+'/params/'+name+'/testX.txt', test_new)
+	np.savetxt(results+'/params/'+name+'/trainX.txt', train_new)
+	return train_new
+
+#def dimensionReduction(train_x, test_x):
+	
+
+def SVM(train_x, train_y, name, results):
+	''' Generate a SVM for each model '''
+	# linear kernel SVM
+	lin_svm = svm.SVC(kernel='linear').fit(train_x, train_y)
+	pickle.dump(lin_svm, open(results+'/params/'+name+'/linear.txt', 'wb'))
+	
+	# Gaussian (RBF) kernel SVM
+	gau_svm = svm.SVC(kernel='rbf').fit(train_x, train_y)
+	pickle.dump(gau_svm, open(results+'/params/'+name+'/gaussian.txt', 'wb'))
+
+	# Polynomial kernel SVM with d = 3
+	pol3_svm = svm.SVC(kernel='poly',degree=3).fit(train_x, train_y)
+	pickle.dump(pol3_svm, open(results+'/params/'+name+'/pol3.txt', 'wb'))
+
+
+	# Polynomial kernel SVM with d = 4
+	pol4_svm = svm.SVC(kernel='poly',degree=4).fit(train_x, train_y)
+	pickle.dump(pol4_svm, open(results+'/params/'+name+'/pol4.txt', 'wb'))
+
+
+	# Polynomial kernel SVM with d = 6
+	pol6_svm = svm.SVC(kernel='poly',degree=6).fit(train_x, train_y)
+	pickle.dump(pol6_svm, open(results+'/params/'+name+'/pol6.txt', 'wb'))
+
+
+if __name__ == '__main__':
+	main()
